284177241d3873c8e8c0fd26ac440089d5160d9d
dikadzikrii <dikadzikri10@gmail.com>
2021-11-15 19:45:08 +0700

    First post

---
+++ src/main/kotlin/BiodataDiri.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val biodataDiri = """
+        Dika Dzikri Pratama
+        21090121
+        Brebes 10 Juli 2004
+        1C
+        Teknik Informatika
+    """.trimIndent()
+
+    println(biodataDiri);
+}
